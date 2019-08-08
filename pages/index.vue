81ef92a5b5176f8dbc1baf409d4cd091fff7621f
Valko Eugene <valkojohn@yandex.ru>
2019-08-08 13:18:21 +0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    indexpage
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-card-text>
+              <v-form>
+                <v-text-field v-model="login" label="Login" type="text" />
+
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  type="password"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    login: '',
+    password: ''
+  })
+}
+</script>
