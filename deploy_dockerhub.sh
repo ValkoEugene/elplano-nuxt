1bb90d7d46426b5e3a205b6d05c5a85c0346076f
Valko Eugene <valkojohn@yandex.ru>
2019-08-13 20:58:09 +0300

    конфигурация для деплоя

---
+++ deploy_dockerhub.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $DOCKER_HUB_REPO:$TAG .
+docker push $DOCKER_HUB_REPO
