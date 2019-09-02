cdbc9002ab417fddbe77866ddac6c8a383abc0aa
Valko Eugene <valkojohn@yandex.ru>
2019-09-03 00:41:57 +0300

    PWA - Добавление приложения на главынй экран

---
+++ plugins/a2hs.js
@@ -0,0 +1,12 @@
+import { namespace, Types } from '../store/user'
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault()
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = e
+  window.$nuxt.$store.commit(
+    `${namespace}/${Types.mutations.SET_SHOW_A2HS_BUTTON}`,
+    true
+  )
+})
