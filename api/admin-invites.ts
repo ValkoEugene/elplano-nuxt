dbce39005609cfe2fa9bd90dc96a585d2f7fe18f
Valko Eugene <valkojohn@yandex.ru>
2019-10-24 23:49:50 +0300

    приглашения в группу на typescript

---
+++ api/admin-invites.ts
@@ -0,0 +1,55 @@
+import axios from '~/plugins/axios'
+
+const restUrl = '/group/invites'
+
+export interface inviteI {
+  email: string
+}
+
+export enum GroupInviteStatuses {
+  accepted = 'accepted',
+  pending = 'pending'
+}
+
+export interface GroupInviteI {
+  email: string
+  invitation_token: string
+  sent_at: string
+  accepted_at: null | string
+  created_at: string
+  updated_at: string
+  status: GroupInviteStatuses
+}
+
+/**
+ * Получить список отправленных приглашений
+ * @async
+ * @type {Function}
+ * @returns {Promise}
+ */
+export async function getInvites(): Promise<GroupInviteI[]> {
+  try {
+    const response = await axios.get(restUrl)
+
+    return response.data.data.map(
+      ({ attributes }: { attributes: GroupInviteI }) => attributes
+    )
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+/**
+ * Отправить приглашение
+ * @async
+ * @type {Function}
+ * @param {inviteI} invite - приглашение
+ * @returns {Promise}
+ */
+export async function sendInvite(invite: inviteI): Promise<void> {
+  try {
+    await axios.post(restUrl, { invite })
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
---
+++ api/user-invites.ts
@@ -0,0 +1,83 @@
+// Работа с приглашениями пользователя
+
+import axios from '~/plugins/axios'
+import { GroupI } from '~/api/group.ts'
+
+const restUrl = '/invites'
+
+export enum GroupInviteStatuses {
+  accepted = 'accepted',
+  pending = 'pending'
+}
+
+export interface GroupInviteI {
+  id: string
+  email: string
+  invitation_token: string
+  sent_at: string
+  accepted_at: string | null
+  created_at: string
+  updated_at: string
+  status: GroupInviteStatuses
+  group_id: string
+  group_title?: string
+  group_number?: string
+}
+
+/**
+ * Получить свойство группы по id
+ * @param groups - список групп
+ * @param prop - название свойтства
+ * @param id - группы
+ */
+function getGroupPropById(
+  groups: { id: string; attributes: GroupI }[],
+  prop: 'title' | 'number',
+  id: string
+): string {
+  const group = groups.find((item) => item.id === id)
+  return group ? group.attributes[prop] : '-'
+}
+
+/**
+ * Получить список приглашений пользователя
+ */
+export async function getInvites(): Promise<GroupInviteI[]> {
+  try {
+    const response = await axios.get(restUrl)
+
+    const invites = response.data.data.map(
+      ({ attributes, relationships }: any) => ({
+        ...attributes,
+        group_id: relationships.group.data.id
+      })
+    )
+    const groups = response.data.included.filter(
+      (item: { type: string }): boolean => item.type === 'group'
+    )
+
+    return invites.map((item: GroupInviteI) => ({
+      ...item,
+      group_title: getGroupPropById(groups, 'title', item.group_id),
+      group_number: getGroupPropById(groups, 'number', item.group_id)
+    }))
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+/**
+ * Принять приглашение в группу
+ * @param token - токен на приглашение
+ */
+export async function acceptInvite(token: string): Promise<void> {
+  try {
+    await axios.put(
+      `${restUrl}/${token}`,
+      {},
+      { headers: { 'Content-Type': 'application/vnd.api+json' } }
+    )
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
