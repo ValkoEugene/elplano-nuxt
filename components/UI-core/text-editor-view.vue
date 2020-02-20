97bfa1b31a97f82eccc143c9656b45f3f1f2655a
Valko Eugene <valkojohn@yandex.ru>
2020-02-20 21:31:05 +0300

    text editor component

---
+++ components/UI-core/text-editor-view.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ql-snow">
+    <div class="ql-editor">
+      <div v-html="value" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class TextEditorVeiw extends Vue {
+  @Prop({ type: String, required: true })
+  private readonly value!: string
+
+  // TODO санитайзинг
+}
+</script>
+
+<style src="quill/dist/quill.snow.css" />
